72284c4bf1682f854f78bbedfc5015db7eca698b
Souravpakhira <pakhira55@gmail.com>
2023-01-08 12:57:01 +0530

    removed

--- choose-skill/src/server.ts
+++ choose-skill/src/server.ts
@@ -1,9 +1,6 @@
 import App from '@/app';
-import AuthRoute from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
-import UsersRoute from '@routes/users.route';
 import validateEnv from '@utils/validateEnv';
-import DomainsRoute from './routes/domain.route';
 import SkillsRoute from '@routes/skill.route';
 import UserSkillRoute from './routes/user-skill.route';
 validateEnv();
